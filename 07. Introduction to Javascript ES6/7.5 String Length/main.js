52fa526f2fd37e460ccb689c46b38eb96bbb5ab7
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-05 10:32:03 +0530

    Tweet Character Limits

---
+++ 07. Introduction to Javascript ES6/7.5 String Length/main.js
@@ -0,0 +1,3 @@
+var tweet = prompt('compose your tweet: ');
+var tweetCount = tweet.length;
+alert('You have written ' + tweetCount + ' characters, ' + 'you have ' + (140 - tweetCount) + ' characters left');
