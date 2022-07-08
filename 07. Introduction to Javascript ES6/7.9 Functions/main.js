76f4dfc2f7bd022456873ba17f1e5079407311ef
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-08 22:24:11 +0530

    Creating and Calling a Function

---
+++ 07. Introduction to Javascript ES6/7.9 Functions/main.js
@@ -0,0 +1,23 @@
+function getMilk(){
+    console.log('leave house');
+    console.log('move right');
+    console.log('move right');
+    console.log('move up');
+    console.log('move up');
+    console.log('move up');
+    console.log('move up');
+    console.log('move right');
+    console.log('move right');
+    console.log('buy milk');
+    console.log('move left');
+    console.log('move left');
+    console.log('move down');
+    console.log('move down');
+    console.log('move down');
+    console.log('move down');
+    console.log('move left');
+    console.log('move left');
+    console.log('enter house');
+}
+
+getMilk();
