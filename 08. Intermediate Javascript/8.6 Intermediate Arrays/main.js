b2c9418a6e12a0388ff95275d3fabf985cdaa303
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-09 23:00:04 +0530

    FizzBuzz

---
+++ 08. Intermediate Javascript/8.6 Intermediate Arrays/main.js
@@ -0,0 +1,29 @@
+var numbers = [];
+var num = 0;
+function fizzBuzz(){
+    num += 1;
+    if(num % 3 == 0){
+        numbers.push('Fizz');
+    } else if(num % 5 == 0){
+        numbers.push('Buzz');
+    } else{
+        numbers.push(num);
+    }
+    console.log(numbers);
+}
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
