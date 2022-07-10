a0dfa6eedc2f977ee018d5906484d5dae8687859
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-10 07:55:39 +0530

    99 Bottles of Beer

---
+++ 08. Intermediate Javascript/8.8 Control Flow - While/main.js
@@ -0,0 +1,6 @@
+var count = 0;
+
+while(count <= 99){
+    console.log((99 - count) + ' bottles of bear on the wall, ' + (99 - count) + ' bottles of beer. Take one down and pass it around, ' + (99 - (count + 1)) + ' bottles of beer on the wall');
+    count++;
+}
