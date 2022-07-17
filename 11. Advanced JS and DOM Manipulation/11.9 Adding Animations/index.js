31d91ec7f7b92c06306a557686579fb1da956100
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-17 08:08:44 +0530

    Animations with setTimeout

---
+++ 11. Advanced JS and DOM Manipulation/11.9 Adding Animations/index.js
@@ -0,0 +1,59 @@
+const buttons = document.querySelectorAll('.drum');
+let tom1 = new Audio('sounds/tom-1.mp3');
+let tom2 = new Audio('sounds/tom-2.mp3');
+let tom3 = new Audio('sounds/tom-3.mp3');
+let tom4 = new Audio('sounds/tom-4.mp3');
+let snare = new Audio('sounds/crash.mp3');
+let crash = new Audio('sounds/crash.mp3');
+let kickBass = new Audio('sounds/kick-bass.mp3');
+
+const makeSound = (key) =>{
+    switch (key) {
+        case 'w':
+            tom1.play();
+            break;
+        case 'a':
+            tom2.play();
+            break;
+        case 's':
+            tom3.play();
+            break;
+        case 'd':
+            tom4.play();
+            break;
+        case 'j':
+            snare.play();
+            break;
+        case 'k':
+            crash.play();
+            break;
+        case 'l':
+            kickBass.play();
+            break;
+        default:
+            console.log(key);
+            break;
+    }
+}
+
+const buttonAnimation = (key) =>{
+    const activeKey = document.querySelector('.' + key);
+    activeKey.classList.add('pressed');
+
+    setTimeout(() =>{
+        activeKey.classList.remove('pressed');
+    }, 100);
+}
+
+buttons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        makeSound(button.innerHTML);
+        buttonAnimation(button.innerHTML);
+    });
+});
+
+document.addEventListener('keydown', e =>{
+    makeSound(e.key);
+    buttonAnimation(e.key);
+});
+
