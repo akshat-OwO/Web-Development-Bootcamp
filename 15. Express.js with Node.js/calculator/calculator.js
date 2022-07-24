8d8e3df65d3faab29396a4184cf447ec00124a4d
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-24 16:53:12 +0530

    Correction

--- 15. Express.js with Node.js/calculator/calculator.js
+++ 15. Express.js with Node.js/calculator/calculator.js
@@ -18,7 +18,7 @@ app.post('/bmicalculator', (req, res) =>{
 
     let bmi = weight / (height ** 2);
 
-    res.send('BMI is ', bmi);
+    res.send('BMI is '+ bmi);
 });
 
 app.post('/', (req, res) =>{
@@ -26,7 +26,7 @@ app.post('/', (req, res) =>{
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
-    res.send('The result of the calculation is ', result);
+    res.send('The result of the calculation is '+ result);
 });
 
 app.listen(3000, () =>{
