cde6e21905722ab81041317f4c03d54302f6280e
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-24 08:12:36 +0530

    Working with Routes

--- 15. Express.js with Node.js/my-express-app/server.js
+++ 15. Express.js with Node.js/my-express-app/server.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) =>{
     res.send('Hello');
 });
 
+app.get('/contact', (req, res) =>{
+    res.send('Contact');
+});
+
+app.get('/about', (req, res) =>{
+    res.send('About');
+});
+
 app.listen(3000, () =>{
     console.log('Started Server on port 3000');
 });
