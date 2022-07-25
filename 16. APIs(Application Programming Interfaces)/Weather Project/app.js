87a6db493dae716625669be948d7693a1929f8a9
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-25 08:25:56 +0530

    Rendering Live API Data

--- 16. APIs(Application Programming Interfaces)/Weather Project/app.js
+++ 16. APIs(Application Programming Interfaces)/Weather Project/app.js
@@ -11,9 +11,16 @@ app.get('/', (req, res) =>{
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<h2> The weather is currently ' + description + ' </h2>');
+            res.write('<h1> The temperature in London is ' + temp + '&#8451 </h1>');
+            res.write(`<img src="${imageURL}">`);
+            res.send();
         });
     });
-    res.send('server is up and running');
 });
 
 
