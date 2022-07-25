cbfee1ef2891bfa6aa1c346aa74cf84483a1e621
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-25 09:17:00 +0530

    Using body-parser to parse the body of the request

--- 16. APIs(Application Programming Interfaces)/Weather Project/app.js
+++ 16. APIs(Application Programming Interfaces)/Weather Project/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) =>{
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=498ef0bbf7dfc787b385e476820cb6d9&units=metric';
+
+    res.sendFile(__dirname + '/index.html');
+    
+});
+
+app.post('/', (req, res) =>{
+    const query = req.body.cityName;
+    const apikey = '498ef0bbf7dfc787b385e476820cb6d9';
+    const unit = 'metric';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+ apikey +'&units=' + unit;
+    
     https.get(url, (response) =>{
         console.log(response.statusCode);
 
@@ -16,7 +30,7 @@ app.get('/', (req, res) =>{
 
             res.setHeader('Content-Type', 'text/html');
             res.write('<h2> The weather is currently ' + description + ' </h2>');
-            res.write('<h1> The temperature in London is ' + temp + '&#8451 </h1>');
+            res.write('<h1> The temperature in '+ query +' is ' + temp + '&#8451 </h1>');
             res.write(`<img src="${imageURL}">`);
             res.send();
         });
