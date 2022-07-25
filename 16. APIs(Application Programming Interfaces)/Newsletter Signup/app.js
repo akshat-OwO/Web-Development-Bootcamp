5da261d5d18744e620ad58f04d02d08b96a3777f
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-25 16:12:37 +0530

    Posting data to Mailchimp's server

--- 16. APIs(Application Programming Interfaces)/Newsletter Signup/app.js
+++ 16. APIs(Application Programming Interfaces)/Newsletter Signup/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -16,9 +17,43 @@ app.post('/', (req, res) =>{
     const lastName = req.body.lName;
     const email = req.body.eMail;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us18.api.mailchimp.com/3.0/lists/eadcc5ba22';
+
+    const options = {
+        method: 'POST',
+        auth: 'akshat:2dace37ba8ee35bd78bb4218413b375d-us18'
+    }
+
+    const request = https.request(url, options, (response) =>{
+        response.on('data', (data) =>{
+            console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen(3000, () =>{
     console.log('server is running on port 3000');
 });
+
+// API key
+// 2dace37ba8ee35bd78bb4218413b375d-us18
+
+// List ID
+// eadcc5ba22
