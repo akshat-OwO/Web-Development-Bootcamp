9703e4fb96ffbff69922324639b76c717474ad23
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-31 17:50:16 +0530

    first ejs template

--- 17. EJS/Todolist-V1/app.js
+++ 17. EJS/Todolist-V1/app.js
@@ -3,13 +3,31 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>{
     let today = new Date();
-    if(today.getDay() == 6 || today.getDay() == 0){
-        res.send("Yay it's weekend");
+    let currentDay = today.getDay();
+    let day = '';
+    if(currentDay == 0){
+        day = 'sunday';
+    } else if(currentDay == 1){
+        day = 'monday';
+    } else if(currentDay == 2){
+        day = 'tuesday';
+    } else if(currentDay == 3){
+        day = 'wednesday';
+    } else if(currentDay == 4){
+        day = 'thursday';
+    } else if(currentDay == 5){
+        day = 'friday';
+    } else{
+        day = 'saturday';
     }
+    
+    res.render('list', { kindOfDay: day });
 });
 
 app.listen(3000, () =>{
