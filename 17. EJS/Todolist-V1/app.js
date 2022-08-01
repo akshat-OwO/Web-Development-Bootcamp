8f3d1ad5caf9f1aa7e65286b458b64c9a491d687
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-01 07:22:09 +0530

    Styling todo list

--- 17. EJS/Todolist-V1/app.js
+++ 17. EJS/Todolist-V1/app.js
@@ -8,6 +8,7 @@ let newItems = ['Buy Food', 'Cook Food', 'Eat Food'];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>{
     let today = new Date();
