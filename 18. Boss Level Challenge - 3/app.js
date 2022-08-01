59639495c983866fbf6da3da21bb1f6a36e11030
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-01 21:33:08 +0530

    Deployment

--- 18. Boss Level Challenge - 3/app.js
+++ 18. Boss Level Challenge - 3/app.js
@@ -54,6 +54,6 @@ app.get('*', (req, res) =>{
   res.status(404).render('404');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
