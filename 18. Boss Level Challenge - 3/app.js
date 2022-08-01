d7d1c68dbf0df1a595ca05bba9ef74e7cbf55581
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-01 20:56:51 +0530

    page 404 and truncating posts body

--- 18. Boss Level Challenge - 3/app.js
+++ 18. Boss Level Challenge - 3/app.js
@@ -52,6 +52,10 @@ app.post('/compose', (req, res) =>{
   res.redirect('/');
 });
 
+app.get('*', (req, res) =>{
+  res.status(404).render('404');
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
