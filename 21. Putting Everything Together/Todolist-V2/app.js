2a78d4dcc7c22b4d68bee73d1061c9c800fbe296
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-03 22:20:08 +0530

    Connecting with mongoose

---
+++ 21. Putting Everything Together/Todolist-V2/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/todoListDB');
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const first = new Item({
+    name: 'Welcome to your todolist'
+});
+
+const second = new Item({
+    name: 'Hit the + button to add a new item'
+});
+
+const third = new Item({
+    name: '<--Hit this to delete an item'
+});
+
+const defaultItems = [first, second, third];
+
+Item.insertMany(defaultItems, (err) =>{
+    if(err){
+        console.log(err);
+    } else{
+        console.log('Successfully added defaultItems');
+    }
+});
+
+app.get('/', (req, res) =>{
+
+    res.render('list', { listTitle: "Today", newListItems: newItems });
+});
+
+app.get('/work', (req, res) =>{
+    res.render('list', { listTitle: 'Work List', newListItems: workItems });
+});
+
+app.post('/', (req, res) =>{
+    let newItem = req.body.newItem;
+
+    if(req.body.list === 'Work'){
+        workItems.push(newItem);
+        res.redirect('/work');
+    } else{
+        newItems.push(newItem);
+        res.redirect('/');
+    }
+
+});
+
+app.post('/work', (req, res) =>{
+    
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect('/work');
+});
+
+app.listen(3000, () =>{
+    console.log('Server is up and running');
+});
