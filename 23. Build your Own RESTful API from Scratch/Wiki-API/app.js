b9963edf001c436aa1f856c6fab27aa70ebe3f35
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-10 14:42:11 +0530

    Setting up server

---
+++ 23. Build your Own RESTful API from Scratch/Wiki-API/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
+
+const articlesSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model('Article', articlesSchema);
+
+app.listen(3000, () =>{
+    console.log('Server is running on port 3000');
+});
