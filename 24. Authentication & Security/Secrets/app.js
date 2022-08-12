1914f19d823cc1a6c6c0e39cbbd28f59a6281f41
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-12 16:06:01 +0530

    Level 1 Security

--- 24. Authentication & Security/Secrets/app.js
+++ 24. Authentication & Security/Secrets/app.js
@@ -5,21 +5,61 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>{
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = {
+    email: String,
+    password: String,
+};
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/login', (req, res) =>{
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.get('/register', (req, res) =>{
+app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.listen(3000, () =>{
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+app.listen(3000, () => {
     console.log('Server is running on PORT 3000');
 });
