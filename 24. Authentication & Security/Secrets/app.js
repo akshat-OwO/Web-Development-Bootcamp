42c6903921a134bceeb28a9f61faa9700132087a
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-14 07:49:19 +0530

    Using Environment Variables

--- 24. Authentication & Security/Secrets/app.js
+++ 24. Authentication & Security/Secrets/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -16,8 +17,10 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-const secret = 'This is our little secret';
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ['password'],
+});
 
 const User = mongoose.model('User', userSchema);
 
