9ecb002355e87c1c20072e880cbd7e567098d240
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-21 08:31:47 +0530

    Introduction to JSX

---
+++ 25. Reactjs/25.1 Introduction to JSX/react-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+    <div>
+        <h1>Hello World</h1>
+        <p>This is a paragraph</p>
+    </div>
+);
