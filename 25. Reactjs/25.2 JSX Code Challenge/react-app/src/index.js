e9e7641c7003bd582f5e7148bea5a01e55c3ff5c
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-21 08:43:12 +0530

    Code Challenge

---
+++ 25. Reactjs/25.2 JSX Code Challenge/react-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+    <div>
+        <h1>My Favourite Foods</h1>
+        <ul>
+            <li>Bacon</li>
+            <li>Noodles</li>
+            <li>Jamon</li>
+        </ul>
+    </div>
+);
