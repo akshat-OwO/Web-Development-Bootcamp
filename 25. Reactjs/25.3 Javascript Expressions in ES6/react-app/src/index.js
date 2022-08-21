21a9580f28023e5af7d69b7214107b3a785d4f59
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-21 09:06:30 +0530

    Code Challenge

--- 25. Reactjs/25.3 Javascript Expressions in ES6/react-app/src/index.js
+++ 25. Reactjs/25.3 Javascript Expressions in ES6/react-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fName = 'Akshat';
-const lName = 'Singh';
-const num = 7;
+const name = 'Akshat';
+const date = new Date();
+const year = date.getFullYear();
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
     <div>
-        <h1>{`Hello ${fName} ${lName}!`}</h1>
-        <p>{`Your Lucky Number is ${num}`}</p>
+        <p>{`Created by ${name}`}</p>
+        <p>{`Copyright ${year}`}</p>
     </div>
 );
