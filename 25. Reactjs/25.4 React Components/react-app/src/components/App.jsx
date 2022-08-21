5174b1cbf97cec3aa0c4834d2f2557634a3972db
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-21 09:34:50 +0530

    Code Challenge

--- 25. Reactjs/25.4 React Components/react-app/src/components/App.jsx
+++ 25. Reactjs/25.4 React Components/react-app/src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Heading from './Heading';
-import List from './List';
 
 const App = () => {
     return ( 
         <div>
             <Heading />
-            <List />
         </div>
      );
 }
