acbaba31a30fc855e7bb61ba601300c1f1eb68a7
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-27 17:08:43 +0530

    Mapping Challenge

---
+++ 25. Reactjs/25.9 Mapping Data Components Practice/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import emojipedia from '../emojipedia';
+import Entry from "./Entry";
+
+function emojisData(emoji){
+  return(
+    <Entry
+        key={emoji.id} 
+        emoji= {emoji.emoji}
+        name= {emoji.name}
+        meaning={emoji.meaning}
+    />
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(emojisData)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
