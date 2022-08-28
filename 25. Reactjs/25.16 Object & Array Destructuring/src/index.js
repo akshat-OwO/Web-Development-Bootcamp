d88b9fada996fde9826bbdf34b1187ae337abeb9
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-28 22:10:32 +0530

    Challenge

--- 25. Reactjs/25.16 Object & Array Destructuring/src/index.js
+++ 25. Reactjs/25.16 Object & Array Destructuring/src/index.js
@@ -1,32 +1,28 @@
-import animals from "./data";
-console.log(animals);
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-const [cat, dog] = animals;
-console.log(cat);
+const [honda, tesla] = cars;
 
-const {name: catName, sound: catSound} = cat;
-console.log(catSound);
+const {coloursByPopularity: [hondaTopColour], speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {coloursByPopularity: [teslaTopColour], speedStats: {topSpeed: teslaTopSpeed}} = tesla;
 
-// CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
